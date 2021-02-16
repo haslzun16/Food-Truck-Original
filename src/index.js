9699411e3f69b7ba95dd029e47be9fc36ae26233
Kahlie1 <39012827+Kahlie1@users.noreply.github.com>
2021-02-16 18:25:30 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export { default as SignIn } from './SignIn'
-
-export { default as BottomNavigation } from './BottomNavigation'
-
-export { default as SignUp } from './SignUp'
-
-export { default as OnBoardingScreen } from './OnBoardingScreen'
