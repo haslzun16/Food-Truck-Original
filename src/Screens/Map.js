cf17d197f63778865bda3ff7fc4a76f2990a2657
Kalob98 <47434477+Kalob98@users.noreply.github.com>
2021-03-09 17:37:42 -0500

    Update Map.js
    Shows user location

--- src/Screens/Map.js
+++ src/Screens/Map.js
@@ -1,21 +1,21 @@
 /**
- * Map Screen
+ * Kalob Reinholz
  *
+ * Last edited 3/7/21
+ * 
  * Shows the user a map with their location and the location of
  * nearby food trucks
  *
+ * npm install -g react-native-cli
+ * npm install react-native-maps
  */
 
-
-
-//import * as React from 'react';
-import React from "react";
+//imports
+import React, { Component } from "react";
 import { Button, View, Text, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
-//import { LinearGradient } from 'expo-linear-gradient';
-import { Component } from 'react';
 import MapView from "react-native-maps";
 
-
+let x = 36.2530474;
 class Map extends Component {
     render() {
         return (
@@ -23,12 +23,21 @@ class Map extends Component {
                 style={{
                     flex: 1
                 }}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}
+                //shows users location
+                showsUserLocation={true}
+                //should follow the user if they move
+                followsUserLocation={true}
+
+            //below allows us to enter a specific location using lat and long
+            /*
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -79.7231278, //-122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }}
+            */
+
             />
         );
     }
