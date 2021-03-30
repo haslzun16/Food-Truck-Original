0363f620eeb04c3f3cd36bc6b7d70f8eecb74d93
Kalob98 <47434477+Kalob98@users.noreply.github.com>
2021-03-30 17:53:16 -0400

    Update Map.js

--- src/Screens/Map.js
+++ src/Screens/Map.js
@@ -2,59 +2,67 @@
  * Kalob Reinholz
  *
  * Last edited 3/7/21
- *
+ * 
  * Shows the user a map with their location and the location of
  * nearby food trucks
  *
  * npm install -g react-native-cli
  * npm install react-native-maps
+ * npm install @react-native-firebase/database
  */
 
 //imports
 import React, { Component } from "react";
-import {
-  Button,
-  View,
-  Text,
-  TextInput,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import * as firebase from 'firebase';
+import { linear } from "react-native/Libraries/Animated/src/Easing";
 
-let x = 36.2530474;
 class Map extends Component {
-  render() {
-    return (
-      <MapView
-        style={{
-          flex: 1,
-        }}
-        //shows users location
-        showsUserLocation={true}
-        //should follow the user if they move
-        followsUserLocation={true}
 
-        //below allows us to enter a specific location using lat and long
-        /*
-            initialRegion={{
-                latitude: 37.78825,
-                longitude: -79.7231278, //-122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-            }}
-            */
-      />
-    );
-  }
+
+    /**
+     * The vendor is the only user that has a MyPage.
+     * There will be a update current location button that will get the users location(lat and long) and push it to the database.
+    */
+
+    /*
+    * TODO
+    * Look at the database and go into the foodtruck location.
+    * populate the users map with food trucks(from the database) near them.
+    */
+
+
+    render() {
+        var latitude = 0;
+        var longitude = 77.0365; //-79.72228;
+
+        /*database()
+            .ref('vendor/1JRC2MPddCVrigMJoNOs2dCU3wL2/location')
+            .once('latitude')
+            .then(snapshot => {
+                latitude = snapshot.val();
+            });*/
+
+        return (
+            <MapView
+                style={{ flex: 1 }}
+                //shows users location
+                showsUserLocation={true}
+                //should follow the user if they move
+                followsUserLocation={true}
+                //button to zoom in on users current location
+                //there is a bug with this button where the button does not show up unless you rotate the phone 
+                showsMyLocationButton={true}
+            >
+
+                <Marker
+                    coordinate={{ latitude, longitude }}
+                //image={require("../../assets/orange-food-truck.png")}
+                />
+
+            </MapView>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  MainView: {
-    flex: 1,
-    alignContent: "center",
-  },
-});
 
 export default Map;
